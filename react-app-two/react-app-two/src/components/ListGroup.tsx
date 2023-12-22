f4465653b43200f92e87e808b5dfc934f557becf
Wallenborg <niklas.wallenborg@hyperisland.se>
2023-12-22 12:21:01 +0100

    list done

--- react-app-two/react-app-two/src/components/ListGroup.tsx
+++ react-app-two/react-app-two/src/components/ListGroup.tsx
@@ -1,4 +1,34 @@
+import { useState } from "react";
+
 function ListGroup() {
-  return <h1>List Group</h1>;
+  let items = ["New", "San", "Toy", "Lon", "Par"];
+  /*  useState is a Hook, a hook is a special function that allows you 
+  to use state and perform side effects in functional components */
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  return (
+    <>
+      <h1>List</h1>
+      {/* If true then the content is rendered if false nothing is rendered */}
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 export default ListGroup;
